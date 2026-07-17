35b0d6c9fe6b81ad2ccb5d7c0c227a2632bc62da
ayush <ayush.mehta010906@gmail.com>
2026-07-17 02:43:52 +0000

    feat: auth and health routes

---
+++ apps/api/src/controllers/health.controller.ts
@@ -0,0 +1,8 @@
+import { Request, Response } from "express";
+
+export function getHealth (_req : Request, res:Response){
+  res.status(200).json({
+    status: "ok",
+    service: "Intellery API"
+  });
+}
